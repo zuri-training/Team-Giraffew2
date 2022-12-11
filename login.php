bb697ee05ce66101eac5a6862e06f3e15ed942fd
Oyekunle Dare <115362169+OyekunleDare@users.noreply.github.com>
2022-12-11 04:25:58 -0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login/signup </title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+
+<img class="flep" src="img/flep.png" alt="">
+<img class="ball1" src="img/Group11.png" alt="">
+<img class="ball2" src="img/Group22.png" alt="">
+<img class="ball3" src="img/Group33.png" alt="">
+<div class="container">
+        <form class="form" id="login" action="action.php" method="POST">
+            <h1 class="form_title">Cardex</h1>
+            <p class="form_title2">Log into your account</p>
+            <?php if(isset($_GET['message']) )
+                echo "<p class='alert alert-danger text-center' align='center' style='color:red'>Invalid Email Or Password</p>";
+            ?>
+			<?php if(isset($_GET['update']) )
+                echo "<p class='alert alert-success text-center' align='center' style='color:green'>Password Reset Successful<br/>Please log in with your new username and password</p>";
+            ?>
+			<?php if(isset($_GET['regsuc']) )
+                echo "<p class='alert alert-success text-center' align='center' style='color:green'>Registration Successful<br/>Please log in with your new username and password</p>";
+            ?>
+            <p>Username</p>
+            <div class="form_message form_message--error"></div>
+            <div class="form_input-group">
+                <input type="email" class="form_input" autofocus placeholder="Username" required name="email">
+                <div class="form_input-error-message"></div>
+            </div>
+            <p>password</p> 
+            <div class="form_input-group">
+                <input type="password" class="form_input" autofocus placeholder="*************" required name="password">
+                <div class="form_input-error-message"></div>
+            </div>
+            <p class="form_textr">
+                <a href="#" class="form_link">Forgot password?</a>
+            </p>
+            <button class="form_button" type="submit" name="login">Log in</button>
+
+            <p class="form_text">OR</p>
+            
+            <button class="form_button2" type="submit">Continue with Google</button>
+            <p class="form_text">
+               Don't have an account? <a class="form_link" href="signup.php" id="linkCreateAccount"> Sign up</a>
+            </p>
+        </form>
+        
+    </div>
+    <script src="js/app.js"></script>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login/signup </title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+
+<img class="flep" src="img/flep.png" alt="">
+<img class="ball1" src="img/Group11.png" alt="">
+<img class="ball2" src="img/Group22.png" alt="">
+<img class="ball3" src="img/Group33.png" alt="">
+<div class="container">
+        <form class="form" id="createAccount" method="POST" action="action.php">
+            <h1 class="form_title2">Create an Account with Cardex</h1>
+            <?php if(isset($_GET['emailexist']) )
+                echo "<p class='alert alert-danger text-center' align='center' style='color:red; font-weight:bold;'>Email already exist in our database</p>";
+            ?>
+            <?php if(isset($_GET['password']) )
+                echo "<p class='alert alert-danger text-center' align='center' style='color:red; font-weight:bold;'>Password and Confirm Password does not match</p>";
+            ?>
+            <div class="form_message form_message--error"></div>
+            <p>Full Name</p>
+            <div class="form_input-group">
+                <input type="text" id="signupUsername" class="form_input" autofocus placeholder="Adeem Onyema Gabriel" name="fullname" required value="<?php if(isset($_POST['fullname'])){echo $_POST['fullname'];}?>">
+                <div class="form_input-error-message"></div>
+            </div>
+            <p>
+                Email
+            </p>
+            <div class="form_input-group">
+                <input type="emaiil" class="form_input" autofocus placeholder="@gmail.com" required name="email">
+                <div class="form_input-error-message"></div>
+            </div>
+            <p>
+                Create Password
+            </p>
+            <div class="form_input-group">
+                <input type="password" class="form_input" autofocus placeholder="Password" required name="password">
+                <div class="form_input-error-message"></div>
+            </div>
+            <p>
+                Confirm Password
+            </p>
+            <div class="form_input-group">
+                <input type="password" class="form_input" autofocus placeholder="Confirm password" required name="conpass">
+                <div class="form_input-error-message"></div>
+            </div>
+             <p class="form_textr">
+              Already have an account?  <a class="form_link" href="login.php" id="linkLogin"> Sign in</a>
+            </p>
+            <button class="form_button" type="submit" name="register">Create Account</button>
+             <p class="form_text">OR</p>   
+            <button class="form_button2" type="submit">Sign up with Google</button>         
+        </form>
+    </div>
+    <script src="js/app.js"></script>
+</body>
+</html>
