2bd1f826b76380548fd1d1bac60b7affe582a743
Oyekunle Dare <115362169+OyekunleDare@users.noreply.github.com>
2022-12-15 00:49:31 +0100

    Update Route.php

--- classes/Route.php
+++ classes/Route.php
@@ -4,7 +4,15 @@ class formController extends UserAuth{
 	private $email;
 	private $password;
 	private $confirmPassword;
-
+	private $nameoffile;
+	private $upload;
+	private $filesizes;
+	private $tmp;
+	private $tmp2;
+	private $pngfilesize;
+	private $pngfilename;
+	
+	
     public function __construct(){
         parent::__construct();
     }
@@ -12,7 +20,7 @@ class formController extends UserAuth{
 	public function handleForm(){
 		switch(true) {
             case isset($_POST['register']):
-                //code to register students 
+                //code to register users 
                 $this->fullname = $_POST['fullname'];
                 $this->email = $_POST['email'];
                 $this->password = $_POST['password'];
@@ -29,6 +37,21 @@ class formController extends UserAuth{
                 //code to logout students 
                 $this->email = $_POST['email'];
                 $this->logout($this->email);
+                break;
+			case isset($_POST['upload']):
+                //code for updating password
+                $this->nameoffile = $_POST['nameoffile'];
+                $this->upload = $_FILES['zipFile']['name'];
+				$this->filesizes = $_FILES['zipFile']['size'];
+				$this->tmp = $_FILES['zipFile']['tmp_name'];
+				$this->pngfilename = $_FILES['pngFile']['name'];
+				$this->tmp2 = $_FILES['pngFile']['tmp_name'];
+				$this->pngfilesize = $_FILES['pngFile']['size'];
+                $this->uploadFile($this->nameoffile, $this->upload, $this->filesizes, $this->tmp, $this->pngfilename, $this->tmp2, $this->pngfilesize);
+                break;
+			case isset($_POST['all']):
+                //code for getting all users
+                $this->getAllCards();
                 break;
             case isset($_POST['reset']):
                 //code for updating password
